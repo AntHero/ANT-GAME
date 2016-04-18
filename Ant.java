d1ac52210b91cda758aadd8d43b2b322a637c8d3
cdooml <cdooml@hotmail.com>
2016-04-18 02:42:59 +0100

    Update Ant.java

--- Ant.java
+++ Ant.java
@@ -1,22 +1,74 @@
 package antgame;
 
 public class Ant {
+	
+	int id;
 	int state;
 	Color color;
-	boolean isResting;
+	int resting;
 	Direction direction;
 	boolean hasFood;
-		
-	public Ant(int state, Color color, boolean isResting, Direction direction, boolean hasFood){
-		this.state = state;
+	boolean isAlive;
+	
+	public Ant(int state, Color color, int resting, Direction direction, boolean hasFood){
+		this.id = id;
+		this.state = 0;
 		this.color = color;
 		this.resting = resting;
-		this.direction = direction;
+		this.direction = new Direction(DirectionType.ZERO);
 		this.hasFood = hasFood;
-		
+		this.isAlive = true;
 		
 		//Test Commit
 	}
-	
-	
+		
+    public void setToDead() {
+        this.isAlive = false;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public void setResting(int resting) {
+        this.resting = resting;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void setHasFood(boolean hasFood) {
+        this.hasFood = hasFood;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getResting() {
+        return resting;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public boolean hasFood() {
+        return hasFood;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+    
+    
 }
