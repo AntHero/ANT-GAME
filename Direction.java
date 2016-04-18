020e4ed533a5086531de7e05f47adcf816d4c24a
cdooml <cdooml@hotmail.com>
2016-04-18 02:49:14 +0100

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -1,37 +1,39 @@
 package antgame;
 
 public class Direction {
-	int direction;
+	DirectionType direction;
 
-	public Direction(int direction) {
+	public Direction(DirectionType direction) {
 		this.direction = direction;
 	}
-
-	public Cell adjacent_cell(Position pos, Direction direction) {
+	
+	//unfinished method
+	public Cell adjacent_cell(Position pos, DirectionType direction) {
 
 		if (pos.getX() % 2 != 0) {
 			switch (direction) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
+			case ZERO:
+        	        case ONE:
+                        case TWO:
+                        case THREE:
+                        case FOUR:
+                        case FIVE:
 		} else {
 			switch (direction) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
+			case ZERO:
+        	        case ONE:
+                        case TWO:
+                        case THREE:
+                        case FOUR:
+                        case FIVE:
 			}
-
+		//unfinished method
+        	return new Cell();
 		}
 		
 
 	}
-		public int getDirection(){
+		public DirectionType getDirection(){
 			return direction;
 		}
 
