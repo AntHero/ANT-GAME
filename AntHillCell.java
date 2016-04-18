3ebaabffa72deebcf9bb9ba39e80326774bb2fb9
cdooml <cdooml@hotmail.com>
2016-04-18 02:45:43 +0100

    Create AntHillCell.java

---
+++ AntHillCell.java
@@ -0,0 +1,15 @@
+package antgame;
+
+
+public class AntHillCell extends ClearCell{
+    
+    Color color;
+    
+    public AntHillCell(int x, int y, int food, Ant ant, Color color) {
+       super(x, y, food);
+       this.ant = ant;
+       this.color = ant.getColor();
+    }
+    
+
+}
