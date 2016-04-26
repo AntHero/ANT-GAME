14dfd9fddeaba81a54104156c78eb64cc62bf8ef
cdooml <cdooml@hotmail.com>
2016-04-26 13:58:43 +0100

    updates
    Previous updates broken the worldparser classes. changed it so both the worldparser and world classes can work with it.

--- AntHillCell.java
+++ AntHillCell.java
@@ -4,14 +4,15 @@ package antgame;
 public class AntHillCell extends ClearCell{
     
     Color color;
-    
+    static int id = 0;
     //take ant parameter out bc you have to id ants from left to right after anthill placed
     public AntHillCell(int x, int y, int food, Color color) {
        super(x, y, food);
-       this.position = new Position (x, y);	//idk
-       this.ant = ant;
+       //this.position = new Position (x, y);	//this already happend in the call to the super constructer.
+       this.ant = new Ant(id, color);
+       id++;
        this.color = ant.getColor();
-       instance = this;
+       instance = this;// not quite sure i understand this
     }
     
 
