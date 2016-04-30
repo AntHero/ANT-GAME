85cd4ab91da05c4c5dedc543afc33cf94b2f0107
cdooml <cdooml@hotmail.com>
2016-05-01 00:40:25 +0100

    Update condition.java

--- condition.java
+++ condition.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package antgame;
 
 /**
- *
- * @author D7ooM
+ *this is mostly used in the sense instruction of the brain.
  */
 public enum condition {
         
