f6c0e44cbc5b4285822a15d9bd94312a2e81e71e
cdooml <cdooml@hotmail.com>
2016-05-01 00:39:32 +0100

    Added files via upload

---
+++ condition.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antgame;
+
+/**
+ *
+ * @author D7ooM
+ */
+public enum condition {
+        
+	Friend, Foe, FriendWithFood, FoeWithFood, Food, Rock, Marker,FoeMarker, Home, FoeHome;
+        
+}
