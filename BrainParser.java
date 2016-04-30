480e87e9379816ea9876266ad7253a6261949a49
cdooml <cdooml@hotmail.com>
2016-05-01 00:37:20 +0100

    Added files via upload

---
+++ BrainParser.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antgame;
+
+import antgame.Instructions.Drop;
+import antgame.Instructions.Flip;
+import antgame.Instructions.Mark;
+import antgame.Instructions.Move;
+import antgame.Instructions.PickUp;
+import antgame.Instructions.Sense;
+import antgame.Instructions.Turn;
+import antgame.Instructions.UnMark;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author D7ooM
+ */
+public class BrainParser {
+
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.out.println(args[0] + " does not exist.");
+            return;
+        }
+        if (!(file.isFile() && file.canRead())) {
+            System.out.println(file.getName() + " cannot be read from.");
+            return;
+        }
+        try {
+            BrainParser parse = new BrainParser();
+            parse.proccessBrain(args[0]);
+        } catch (IOException e) {
+        }
+    }
+
+    /* the method creates an array list constisting of instructions taken from
+     *a parsed ant brain file
+     */
+    public ArrayList proccessBrain(String fileName) throws FileNotFoundException, IOException {
+
+        File file = new File(fileName);
+        ArrayList brain = new ArrayList();
+        String current;
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        while ((current = reader.readLine()) != null) {
+            //System.out.println(current);
+            String[] s = current.split(" ");
+            int st1;
+            int st2;
+            int i;
+            Markers m;
+            int st;
+            int afterMarker;
+
+            if (s[0].equals("Sense")) {
+                sense_dir cuSen = sense_dir.valueOf(s[1]);
+                st1 = Integer.parseInt(s[2]);
+                st2 = Integer.parseInt(s[3]);
+                condition cond = condition.valueOf(s[4]);
+                if (cond.equals(condition.Marker)) {
+                    afterMarker = Integer.parseInt(s[5]);
+                    brain.add(new Sense(cuSen, st1, st2, cond, afterMarker));
+                } else {
+                    brain.add(new Sense(cuSen, st1, st2, cond));
+                }
+            } else if (s[0].equals("Mark")) {
+                i = Integer.parseInt(s[1]);
+                m = new Markers(i);
+                st = Integer.parseInt(s[2]);
+                brain.add(new Mark(m, st));
+            } else if (s[0].equals("Unmark")) {
+                i = Integer.parseInt(s[1]);
+                m = new Markers(i);
+                st = Integer.parseInt(s[2]);
+                brain.add(new UnMark(m, st));
+            } else if (s[0].equals("PickUp")) {
+                st1 = Integer.parseInt(s[1]);
+                st2 = Integer.parseInt(s[2]);
+                brain.add(new PickUp(st1, st2));
+            } else if (s[0].equals("Drop")) {
+                st = Integer.parseInt(s[1]);
+                brain.add(new Drop(st));
+            } else if (s[0].equals("Turn")) {
+                Left_or_Right lr = Left_or_Right.valueOf(s[1]);
+                st = Integer.parseInt(s[2]);
+                brain.add(new Turn(lr, st));
+            } else if (s[0].equals("Move")) {
+                st1 = Integer.parseInt(s[1]);
+                st2 = Integer.parseInt(s[2]);
+                brain.add(new Move(st1, st2));
+            } else if (s[0].equals("Flip")) {
+                i = Integer.parseInt(s[1]);
+                st1 = Integer.parseInt(s[2]);
+                st2 = Integer.parseInt(s[3]);
+                brain.add(new Flip(i, st1, st2));
+            } else {
+                System.out.println("incorect istruction detected");
+            }
+
+        }
+        return brain;
+
+    }
+}
---
+++ Instructions.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antgame;
+
+/**
+ *This is an interface which links all the seven instruction under it.
+ *This we be used in the brain parser to store the text file of the brain in a List.
+ */
+public interface Instructions {
+
+    public class Sense implements Instructions {
+        sense_dir p;
+        int st1;
+        int st2;
+        condition c;
+        int markerNum;
+        public Sense(sense_dir x, int stateNum1, int stateNum2, condition cond) {
+            this.p = x;
+            this.st1 = stateNum1;
+            this.st2 = stateNum2;
+            this.c = cond;
+        }
+
+        Sense(sense_dir x, int stateNum1, int stateNum2, condition cond, int afterMarker) {
+            this.p = x;
+            this.st1 = stateNum1;
+            this.st2 = stateNum2;
+            this.c = cond;
+            this.markerNum = afterMarker;
+        }
+    }
+
+    public class Mark implements Instructions {
+
+        Markers i;
+        int st;
+        public Mark(Markers x, int stateNum) {
+            this.i = x;
+            this.st = stateNum;
+        }
+    }
+
+    public class UnMark implements Instructions {
+        Markers i;
+        int st;
+        public UnMark(Markers x, int stateNum) {
+            this.i = x;
+            this.st = stateNum;
+        }
+    }
+
+    public class PickUp implements Instructions {
+        int st1;
+        int st2;
+        public PickUp(int stateNum1, int stateNum2) {
+            this.st1 = stateNum1;
+            this.st2 = stateNum2;
+        }
+    }
+
+    public class Drop implements Instructions {
+        int st;
+        public Drop(int stateNum) {
+            this.st = stateNum;
+        }
+    }
+
+    public class Turn implements Instructions {
+        Left_or_Right lr;
+        int st2;
+        public Turn(Left_or_Right lr, int stateNum) {
+            this.lr = lr;
+            this.st2 = stateNum;
+        }
+    }
+
+    public class Move implements Instructions {
+        int st1;
+        int st2;
+        public Move(int stateNum1, int stateNum2) {
+            this.st1 = stateNum1;
+            this.st2 = stateNum2;
+        }
+    }
+
+    public class Flip implements Instructions {
+        int p;
+        int st1;
+        int st2;
+        public Flip(int x, int stateNum1, int stateNum2) {
+            this.p = x;
+            this.st1 = stateNum1;
+            this.st2 = stateNum2;
+        }
+
+    }
+}
