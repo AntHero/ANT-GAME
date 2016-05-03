d35b6518ebfb5016c2dbdaa68f0ab47f37ebcace
Jackj888 <JackAyrtonJames@gmail.com>
2016-05-04 00:29:45 +0100

    Update RockyCell.java
    Javadoc

--- RockyCell.java
+++ RockyCell.java
@@ -1,7 +1,14 @@
 package antgame;
-
+/**
+ * This class represents a rocky cell in the simulation
+ * @author Arco James
+ */
 public class RockyCell extends Cell {
-        
+        /**
+         * The constructor of RockyCell
+         * @param x The 'X' coordinate of the cell
+         * @param y The 'Y' coordinate of the cell
+         */
     public RockyCell(int x, int y){
         this.position = new Position(x,y);
         instance = this;
