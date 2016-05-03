c9831c3f90e535f07a5f87c6998046bce50499a1
Jackj888 <JackAyrtonJames@gmail.com>
2016-05-03 23:53:41 +0100

    Update Left_or_Right.java
    Javadoc Style Comments

--- Left_or_Right.java
+++ Left_or_Right.java
@@ -1,6 +1,9 @@
 package antgame;
-
-//enum for left or right
+/**
+ * An enum to store the values 'Left' and 'Right' to be used by the other classes.
+ * 
+ * @author Arco
+ */
 public enum Left_or_Right {
 	Left, Right;
 }
