daee29cf4d5fd3d6dc21f1a7b5e6f2d9e097003d
Jackj888 <JackAyrtonJames@gmail.com>
2016-05-03 22:59:27 +0100

    Update GameSimulator.java
    Javadoc-esque comments

--- GameSimulator.java
+++ GameSimulator.java
@@ -3,14 +3,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-
+/**
+ * This is the class which acts as the simulation. 
+ * It uses the other class to create the ant game.
+ * @author Abdulrahman
+ */
 public class GameSimulator {
 	
 	//alright this class should be where everything comes together
 	//string arguments should go either
 	//brain1 brain2	randomworldname seed OR
 	//brain1 brain2 world 
-	
+	/**
+	 * Main argument uses the ant brain and map files as arguments
+	 * reads in the two brains and the map.
+	 * @param args The list of arguments
+	 */
     public static void main(String[] args) throws FileNotFoundException, IOException {
     	File arg1;
     	File arg2;
